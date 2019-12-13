d1fba08f510e6a41fd750d35ab9eb062af6f44a3
Strahinja Laktovic <strahinja.laktovic@gmail.com>
2019-12-13 19:53:44 +0100

    sagas fix

--- src/store/sagas.ts
+++ src/store/sagas.ts
@@ -1,6 +1,12 @@
-import { all } from 'redux-saga/effects';
+import { all, fork } from 'redux-saga/effects';
 
-function* rootSaga() {
-  yield all([]);
+const sagas = [];
+
+function* globalSagas() {
+  const globalSagasForks = sagas.map(saga => fork(saga));
+
+  yield all([...globalSagasForks]);
 }
-export default rootSaga;
+
+export default globalSagas;
+
